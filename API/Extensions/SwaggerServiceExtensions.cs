ae16e722e3a10b87b0a9a0b2b6b177564af8e6f1
Rafael Terzi <snarfelcv@gmail.com>
2023-02-07 15:41:32 -0500

    End of Section 16

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -10,6 +10,34 @@ namespace API.Extensions
     {
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c => 
+            {
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorisation",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirements = new OpenApiSecurityRequirement
+                {
+                    {
+                        securitySchema, new[] {"Bearer"}
+                    }
+                };
+
+                c.AddSecurityRequirement(securityRequirements);
+            });
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" }));
 
             return services;
--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -15,6 +15,10 @@ namespace API.Helpers
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
             
             CreateMap<Address, AddressDTO>().ReverseMap();
+
+            CreateMap<CustomerBasketDTO, CustomerBasket>();
+
+            CreateMap<BasketItemDTO, BasketItem>();
         }
     }
 }
