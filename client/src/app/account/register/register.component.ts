de4c3a8e5d7c46c8eaba57b15b57a9d420b4bdc6
Rafael Terzi <snarfelcv@gmail.com>
2023-02-09 16:17:49 -0500

    mmiddle of section 16

---
+++ client/src/app/account/register/register.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize, map } from 'rxjs/operators';
+import { AccountService } from '../account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  errors: string[] | null = null;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
+
+  complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['',[ Validators.required, Validators.email], [this.validateEmailNotTaken()]],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]]
+  })
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: error => this.errors = error.errors
+    })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return this.accountService.checkEmailExists(control.value).pipe(
+        map(result => result ? {emailExists: true} : null),
+        finalize(() => control.markAsTouched())
+      )
+    }
+  }
+}
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -9,6 +9,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
@@ -20,7 +21,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
       preventDuplicates: true
     }),
     BreadcrumbModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    SharedModule
   ],
   exports: [
     NavBarComponent,
